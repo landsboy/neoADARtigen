22c469a1a21fc2a6b2d23e45e48893c40a39a5a7
Netanel Landesman <116167793+landsboy@users.noreply.github.com>
2024-05-05 11:50:55 +0300

    Update 5.5.24

--- find_seq_of_mutation.sh
+++ find_seq_of_mutation.sh
@@ -5,8 +5,9 @@ chr="$1"
 mut_position="$2"
 start_seq=$((mut_position-$3))
 end_seq=$((mut_position+$3))
+k="$4"
 # path to create temp file of BED format for query
-out_temp="/home/alu/netlandes/MHCpan/TEMP.bed"
+out_temp="/home/alu/netlandes/MHCpan/temp_folder/TEMP${k}.bed"
 # create the temp file with the BED entry
 printf "%s\t%s\t%s\n" "$chr" "$start_seq" "$end_seq" > "$out_temp"
 # extract the sequence and print it
