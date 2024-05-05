9825269f0af596419ae53b0369e64854f50afdbe
Netanel Landesman <116167793+landsboy@users.noreply.github.com>
2024-05-05 11:51:26 +0300

    Update 5.5.24

--- find_position.sh
+++ find_position.sh
@@ -6,10 +6,11 @@
 genome="/home/alu/netlandes/MHCpan/bed6_of_ensemble.bed"
 chr="$1"
 mut_position="$2"
+k="$3"
 start_seq=$((mut_position-15))
 end_seq=$((mut_position+15))
 # path to create temp file of BED format for query
-out_temp="/home/alu/netlandes/MHCpan/TEMP.bed"
+out_temp="/home/alu/netlandes/MHCpan/temp_folder/TEMP${k}.bed"
 # create the temp file with the BED entry  $genome
 printf "%s\t%s\t%s\n" "$chr" "$start_seq" "$end_seq" > "$out_temp"
-bedtools intersect -wa -a "$genome" -b "$out_temp" > "/home/alu/netlandes/MHCpan/intersects_output.bed"
+bedtools intersect -wa -a "$genome" -b "$out_temp" 
